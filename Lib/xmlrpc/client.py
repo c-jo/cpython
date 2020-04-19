054b478fb07f974d4b21ced1ae150f2b6e892ad7
Chris Johns <chris@lessthan3.org.uk>
2020-04-19 12:12:09 +0100

    Handle time on RISC OS in xmlrpc client

--- Lib/xmlrpc/client.py
+++ Lib/xmlrpc/client.py
@@ -263,18 +263,21 @@ boolean = Boolean = bool
 
 
 # Issue #13305: different format codes across platforms
-_day0 = datetime(1, 1, 1)
-if _day0.strftime('%Y') == '0001':      # Mac OS X
+if sys.platform == 'riscos':
     def _iso8601_format(value):
         return value.strftime("%Y%m%dT%H:%M:%S")
-elif _day0.strftime('%4Y') == '0001':   # Linux
-    def _iso8601_format(value):
-        return value.strftime("%4Y%m%dT%H:%M:%S")
 else:
-    def _iso8601_format(value):
-        return value.strftime("%Y%m%dT%H:%M:%S").zfill(17)
-del _day0
-
+    _day0 = datetime(1, 1, 1)
+    if _day0.strftime('%Y') == '0001':      # Mac OS X
+        def _iso8601_format(value):
+            return value.strftime("%Y%m%dT%H:%M:%S")
+    elif _day0.strftime('%4Y') == '0001':   # Linux
+        def _iso8601_format(value):
+            return value.strftime("%4Y%m%dT%H:%M:%S")
+    else:
+        def _iso8601_format(value):
+            return value.strftime("%Y%m%dT%H:%M:%S").zfill(17)
+    del _day0
 
 def _strftime(value):
     if isinstance(value, datetime):
